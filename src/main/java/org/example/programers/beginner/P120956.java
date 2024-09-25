d28f910a24c9f82f49cf0bdb62cea32b8a72b609
gh3298 <gh3298@gmail.com>
2024-09-25 15:09:45 +0900

    .

---
+++ src/main/java/org/example/programers/beginner/P120956.java
@@ -0,0 +1,27 @@
+package org.example.programers.beginner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class P120956 {
+
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] keyWord = {"aya", "ye", "woo", "ma"};
+        ArrayList<String> list = new ArrayList<>();
+
+        boolean isExist = false;
+
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[] babbling = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
+
+        P120956 sol = new P120956();
+        sol.solution(babbling);
+    }
+}
